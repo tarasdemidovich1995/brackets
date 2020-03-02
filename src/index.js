c0194efea273b9e628f5ab2f6ee0ec3b9e21aa1d
Taras Demidovich <tarasdemidovich1995@gmail.com>
2020-03-02 23:05:59 +0300

    feat: add solution to task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    bracketsConfig = bracketsConfig.map(elem => elem.map(elem => isNaN(elem) ? '\\' + elem : elem).join(''));
+    for (let i = 0; i < bracketsConfig.length;) {
+        const regexp = new RegExp(bracketsConfig[i], 'g');
+        const regexpTest = new RegExp(bracketsConfig[i]);
+        if (regexpTest.test(str)) {
+            str = str.replace(regexp, '');
+            i = 0;
+        } else {
+            i++;
+        }
+    }
+    return str.length == 0;
 }
+
